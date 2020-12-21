7e7a92c7874d090e43036a9baa144e67f1afd6a4
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2020-12-21 17:56:55 +0500

    redux with toolkit

---
+++ redux-with-toolkit/src/components/Counter.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import {useDispatch, useSelector} from 'react-redux'
+import { increment, decrement, incrementByAmount } from '../store/counterSlice';
+
+
+function Counter() {
+    const [value, setValue] = useState(0);
+    const dispatch = useDispatch();
+    const counter = useSelector( (state)=> {
+        console.log(state);
+        return state.counter.count
+    });
+  return (
+    <div >
+        <div>Counter: {counter}</div>
+        <div>
+            <button onClick={()=>{
+                dispatch( increment() );
+            }}>Increment</button>
+            <br/>
+            <button onClick={()=>{
+                dispatch( decrement()  );
+            }}>Decrement</button>
+        </div>
+        <div>
+            <input type="text" onChange={(e)=> {
+                setValue(e.target.value);
+            }} />
+            <br/>
+            <button onClick={()=>{
+                dispatch(incrementByAmount(Number(value)))
+            }}>Increment By Value</button>
+        </div>
+    </div>
+  );
+}
+
+export default Counter;
